a2688fe8ff3430c105df6cb6fba2dcd6e25798e2
Scott <scott.kvitberg@gmail.com>
2016-03-11 11:14:01 +0100

    Added layout for the main menu as well as for the the multiple choice menu

--- JapanQuiz/SettingsViewController.swift
+++ JapanQuiz/SettingsViewController.swift
@@ -10,10 +10,59 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet var gameModeButtons: [UIButton]!
+    
+    @IBAction func gameModeButtonHandler(sender: UIButton) {
+        for button in gameModeButtons{
+            if sender != button {
+                button.alpha = 0.7
+            } else {
+                button.alpha = 1.0
+                gameMode = button.tag
+            }
+        }
+    }
+    
+    @IBAction func playButtonHandler(sender: UIButton) {
+        var vc: UIViewController?
+        switch gameMode {
+        case 0:
+            //Multiple Choice quiz
+            vc = storyboard?.instantiateViewControllerWithIdentifier("multipleChoiceViewController") as! MultipleChoiceViewController
+            break
+        case 1:
+            //Right or wrong quiz
+            vc = storyboard?.instantiateViewControllerWithIdentifier("rightOrWrongViewController") as! RightOrWrongViewController
+            break
+        case 2:
+            //Single Choice quiz
+            vc = storyboard?.instantiateViewControllerWithIdentifier("inputViewController") as! InputViewController
+            break
+        case 3:
+            //Image quiz
+            vc = storyboard?.instantiateViewControllerWithIdentifier("imageViewController") as! ImageViewController
+            break
+        default:
+            break
+        
+        }
+        if vc != nil{
+            navigationController?.pushViewController(vc!, animated: true)
+        }
+    }
+    
+    var gameMode = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        for (idx, button) in gameModeButtons.enumerate(){
+            button.tag = idx
+            if button.tag != 0 {
+                button.alpha = 0.7
+            }
+        }
     }
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
